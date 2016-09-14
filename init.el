65004f2a458299d29cab71f68594ce8fda50f76c
Jonix <jonatan@jonix.org>
2016-09-14 10:11:02 +0200

    This is a half a year update to Emacs configuration
    I have been too forgetful to commit Emacs configuration
    to Git.
    
    So this is what I use today, and have actively evolved during
    a 6+ month development of my Emacs configuration.
    
    The reason behind that I haven't commited my configuration is
    that I was waiting to have my Git server setup.
    
    I am still waiting for that, BUT I use one of Git's killer
    feature to version my stuff locally. This is NOT CVS, nor Subversion

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
 ;; Always load newest byte code
-(setq load-prefer-newer t)
+(setq lllload-prefer-newer t)
 
 (when (>= emacs-major-version 24)
   (require 'package)
@@ -17,7 +17,7 @@
 (require 'org)
 (org-babel-load-file
 (expand-file-name "settings.org"
-		  user-emacs-directory))
+                  user-emacs-directory))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -25,10 +25,23 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "68d36308fc6e7395f7e6355f92c1dd9029c7a672cbecf8048e2933a053cf27e6" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default))))
+    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "68d36308fc6e7395f7e6355f92c1dd9029c7a672cbecf8048e2933a053cf27e6" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default)))
+ '(doc-view-continuous t)
+ '(ledger-reports
+   (quote
+    (("U" "ledger -f 2016.ledger")
+     ("Utgifter" "ledger ")
+     ("2016.ledger" "ledger ")
+     ("jonix" "ledger -f 2016.ledger report")
+     ("frotz" "ledger ")
+     ("bal" "ledger -f %(ledger-file) bal")
+     ("reg" "ledger -f %(ledger-file) reg")
+     ("payee" "ledger -f %(ledger-file) reg @%(payee)")
+     ("account" "ledger -f %(ledger-file) reg %(account)")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
+(put 'upcase-region 'disabled nil)
